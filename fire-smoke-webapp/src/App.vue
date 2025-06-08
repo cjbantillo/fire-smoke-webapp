a72137dfeda93f74b986d87efcfdbdd6269be9b6
cjbantillo <bantillocj1@example.com>
2025-06-08 15:49:27 +0800

    first commit

---
+++ fire-smoke-webapp/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Home from './components/Home.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <Home/>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
---
+++ fire-smoke-webapp/src/components/Home.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template>
+  <div>
+    <h1>Fire Smoke Web App 🔥</h1>
+    <h3>This is a simple web application to demonstrate fire-smoke-detection</h3>
+    <br>
+    <h>Credits to the creator: Sayed Gamal</h>
+    <p>https://github.com/sayedgamal99/Real-Time-Smoke-Fire-Detection-YOLO11</p>
+    <br>
+    <p>Current time: {{ new Date().toLocaleTimeString() }}</p>
+    <button>Click to run Live Camera</button>
+  </div>
+</template>
+
+<style scoped></style>
